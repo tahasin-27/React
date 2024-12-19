cdfc3baeccb59d64daee379b6197de9788c6b85d
tahasin305 <tahasinnowar1@gmail.com>
2024-12-19 21:50:58 +0600

    file structure

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+      <h1>classic react</h1>
+      <Youtube/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Youtube from "./Youtube.jsx"
+
+
+function App() {
+
+  return (
+    <>
+    <h1>vite react js</h1>
+    <h1>vite react js</h1>
+    <Youtube/>
+    </>   
+  )
+}
+
+export default App
