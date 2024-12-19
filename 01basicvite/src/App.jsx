84d3cd0307d1b6878e7e8032b794559e80d7ad55
tahasin305 <tahasinnowar1@gmail.com>
2024-12-19 23:22:35 +0600

    jsx

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -4,10 +4,11 @@ import Youtube from "./Youtube.jsx"
 
 
 function App() {
+  const username = "Tahasin"
 
   return (
     <>
-    <h1>vite react js</h1>
+    <h1>vite {" "}react js {username}</h1>
     <h1>vite react js</h1>
     <Youtube/>
     </>   
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,7 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https: google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href='http://google.com' target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: "https://google.com", target: "_blank"},
+    'click to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
     <App />
+    // <MyApp/>
+    // AnotherElement
+    // areactElement
 )
